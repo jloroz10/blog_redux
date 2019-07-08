3304ce89ebb46b8ca264deeb14e11953395eb8f9
joseoroz <joseoroz@JOSEOROZ-LAP.oradev.oraclecorp.com>
2019-07-08 12:25:45 -0700

    adding publication logic to get the user name

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 function Menu(){
     return(
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
             <a className="navbar-brand" href="#">Navbar</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
--- src/components/Publications/index.js
+++ src/components/Publications/index.js
@@ -1,10 +1,41 @@
 import React,{Component} from 'react';
+import {connect} from 'react-redux'
+
+import * as usersActions from '../../actions/usersActions';
 
 class Publications extends Component{
+
+    componentDidMount(){
+        //se tratara de leer la informacion de los usuarios solo cuando este vacio el arrlego,
+        //si lo esta quiere decir que se refresco la pagina estando en publications
+        if(!this.props.users.length){
+            console.log('users is empty');
+            this.props.traerTodos();
+        }
+    }
+
+    mostrarUserName(){
+        console.log(this.props);
+        if(this.props.users.length>0){
+            const userId = this.props.match.params.index;
+            console.log(this.props.users[userId].name);
+            return  this.props.users[userId].name;
+            
+        }
+        else return '';
+    }
     render(){
-        console.log(this.props.match.params.index);
-        return(<h1>Pubs</h1>);
-       
+      
+        return(     
+            <div className="container">
+                <h1>Publicaciones de {this.mostrarUserName()}</h1>
+            </div>
+
+       );
     }
 }
-export default Publications;
+
+const mapStateToProps = reducers => reducers.usersReducer;
+
+
+export default connect(mapStateToProps,usersActions)(Publications);
